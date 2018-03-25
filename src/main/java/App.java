b468a068c036b64b1418151c1b917ef602f82e52
Ioana Cioata <ioana.cioata26@gmail.com>
2018-03-25 16:42:00 +0300

    Added stuff

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,8 +6,8 @@ import controller.DoctorController;
 public class App {
 
 	public static void main(String[] args) {
-		String patients = "FilePatients.txt";
-		String consultations = "FileConsultations.txt";
+		String patients = "E:\\workspace\\vvss\\Lab 1\\Prj_1_FamilyDoctor\\FilePatients.txt";
+		String consultations = "E:\\workspace\\vvss\\Lab 1\\Prj_1_FamilyDoctor\\FileConsultations.txt";
 		Repository repo = new Repository(patients, consultations);
 		DoctorController ctrl = new DoctorController(repo);
 		
